52da2489dfc0cad49a958c44c85e367a58303217
Kakadu123 <s.gaspar.85@gmail.com>
2015-06-28 20:25:13 +0200

    style: Remove trailing spaces

--- tournament.sql
+++ tournament.sql
@@ -6,15 +6,15 @@
 
 -- Table with player details
 
-CREATE TABLE players ( 
+CREATE TABLE players (
 			id SERIAL PRIMARY KEY,
 			name TEXT
                       );
 
 -- Table with match details
 
-CREATE TABLE matches ( 
-			id SERIAL PRIMARY KEY, 
+CREATE TABLE matches (
+			id SERIAL PRIMARY KEY,
 			winner INTEGER REFERENCES players (id),
 			loser INTEGER REFERENCES players (id),
                       );
@@ -23,38 +23,38 @@ CREATE TABLE matches (
 
 CREATE VIEW wins AS
 			
-			SELECT 
-				winner, 
-				count(winner) 
-			FROM 
-				matches 
-			GROUP BY 
-				winner 
+			SELECT
+				winner,
+				count(winner)
+			FROM
+				matches
+			GROUP BY
+				winner
 ;
 
 -- View counting loses by player
 
 CREATE VIEW loses AS
 			
-			SELECT 
-				loser, 
-				count(loser) 
-			FROM 
-				matches 
-			GROUP BY 
-				loser 
+			SELECT
+				loser,
+				count(loser)
+			FROM
+				matches
+			GROUP BY
+				loser
 ;
 
 -- View counting matches by player
 
 CREATE VIEW matchesByPlayer AS
 			
-			SELECT 
-				p.id, 
+			SELECT
+				p.id,
 				p.name,
-				0 + COALESCE(w.count, 0)  +  COALESCE(l.count, 0) AS Count            
-			FROM 
-				players AS p 
+				0 + COALESCE(w.count, 0) + COALESCE(l.count, 0) AS Count
+			FROM
+				players AS p
 			LEFT JOIN
 				wins AS w
 			ON w.winner = p.id
@@ -67,16 +67,16 @@ CREATE VIEW matchesByPlayer AS
 
 CREATE VIEW playerStanding AS
 			
-			SELECT 
-				m.id, 
-				m.name, 
-				COALESCE(w.count, 0) AS wins, 
-				m.Count AS matches 
-			FROM 
-				matchesByPlayer AS m 
-			LEFT JOIN 
-				wins AS w 
-			ON w.winner = m.id 
-			ORDER BY 
+			SELECT
+				m.id,
+				m.name,
+				COALESCE(w.count, 0) AS wins,
+				m.Count AS matches
+			FROM
+				matchesByPlayer AS m
+			LEFT JOIN
+				wins AS w
+			ON w.winner = m.id
+			ORDER BY
 				wins DESC
 ;
